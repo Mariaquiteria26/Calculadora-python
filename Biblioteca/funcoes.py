5dc01345f2e30e0a357a44d1f27425ed384349ee
Mariaquiteria26 <mariahquiteriaa@gmail.com>
2025-10-28 10:56:46 -0300

    Nova questão

---
+++ Biblioteca/funcoes.py
@@ -0,0 +1,10 @@
+def addlivro(lista):
+    livro = {
+        "titulo" : ''
+        "autor" : ''
+        "status" : ''
+
+}
+livro["titulo"] = input("Digite o titulo do livro: ").strip().lower()
+livro["autor"] = 
+
