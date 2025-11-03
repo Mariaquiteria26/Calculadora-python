f6f7df4bf4fa022c96e22423116ac3edfd7a24ed
IvensAr7 <ivens.silva1@aluno.ce.gov.br>
2025-11-03 15:57:08 -0300

    aaa

--- Gerencidor-notas/programa.py
+++ Gerencidor-notas/programa.py
@@ -1,7 +1,10 @@
 from funcoes import *
+from pandas import *
 
+lista = []
 
 while True:
+    
     print('''
 [1] Cadastrar aluno e notas
 [2] Exibir relatório
@@ -9,16 +12,24 @@ while True:
 ''')
     
     while True:
+        dicio = {
+        'nome'  : '',
+        'notas' : [],
+        'media' : 0
+        }
         op = input("escolha a sua opção: ")
         if op not in ['0', '1', '2']:
             print("opção inválida")
             continue
         break
+    
     if op == '0':
         break
+    
     elif op == '1':
-        listas = notas = []
+        notas = []
         nome = input("digite o nome do aluno: ")
+        
         for i in range(3):
             while True:
                 try:
@@ -26,3 +37,12 @@ while True:
                     break
                 except ValueError:
                     print("erro, valor inválido.")
+                    
+        dicio['nome'] = nome
+        dicio['notas'] = notas
+        dicio['media'] = calcular_media(notas)
+        lista.append(dicio)
+        
+    elif op == '2':
+        tabela = DataFrame(lista)
+        print(tabela)
